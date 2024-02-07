aa3085308c8c48b39da2fc9d5e3dc2ba08b5fd39
Matheus Ribeiro <mathribe2020@gmail.com>
2024-02-07 00:50:41 -0300

    feat: add github repository link

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { ExternalLink, Github } from 'lucide-react';
 import logoNlwExpert from './assets/logo-nlw-expert2.svg';
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
@@ -7,7 +8,18 @@ export function App() {
 	return (
 		<>
 			<div className='mx-auto max-w-6xl my-12 px-4 space-y-6'>
-				<img src={logoNlwExpert} alt='logo' className='h-6' />
+				<div className='flex justify-between flex-1'>
+					<img src={logoNlwExpert} alt='logo' className='h-6' />
+
+					<a
+						className='flex items-center gap-2 text-slate-500 hover:text-slate-400 cursor-pointer transition-all duration-200'
+						href='https://github.com/mathrb22/nlw-expert-notes'
+						target='_blank'>
+						<Github className='h-5' />
+						Github
+						<ExternalLink className='h-5' />
+					</a>
+				</div>
 
 				<form className='w-full'>
 					<SearchBar />
